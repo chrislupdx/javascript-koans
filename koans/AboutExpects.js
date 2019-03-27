c8a1aa19f4557e2a45e6dd6cbd8e549c61b87cfd
dreamer <wildetangeant@gmail.com>
2019-03-27 12:36:47 -0700

    about arrays finished

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -17,7 +17,7 @@ describe('About Expects', function() {
 
   // Some ways of asserting equality are better than others.
   it('should assert equality a better way', function() {
-    var expectedValue = FILL_ME_IN;
+    var expectedValue = 2;
     var actualValue = 1 + 1;
 
   // toEqual() compares using common sense equality.
@@ -26,7 +26,7 @@ describe('About Expects', function() {
 
   // Sometimes you need to be precise about what you "type."
   it('should assert equality with ===', function() {
-    var expectedValue = FILL_ME_IN;
+    var expectedValue = '2';
     var actualValue = (1 + 1).toString();
 
   // toBe() will always use === to compare.
@@ -34,7 +34,7 @@ describe('About Expects', function() {
   });
 
   // Sometimes we will ask you to fill in the values.
-  it('should have filled in values', function() {
-    expect(1 + 1).toEqual(FILL_ME_IN);
+  it('should have fillIf we wanna query these APIs at scale, we might need an organized way to guess which companies have ed in values', function() {
+    expect(1 + 1).toIf we wanna query these APIs at scale, we might need an organized way to guess which companies have ('Fill this value in');
   });
 });
